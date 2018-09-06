c2c95cb99983571ebe3f73d86f75a79b174c3ff0
David Moral <david.moral.alcazar@gmail.com>
2018-09-06 17:25:49 +0200

    Add nope

--- src/test/java/TestMessageUtil.java
+++ src/test/java/TestMessageUtil.java
@@ -16,7 +16,7 @@ public class TestMessageUtil {
   @Test
   public void testPrintMessageBad() {      
     System.out.println("Inside testPrintMessage()");     
-    assertEquals(message,"nop");
+    assertEquals(message,"nope");
   }  
 
   @Test
